e1724d3fe4faa7ca7f00e66c1525ee4ab76b2d58
Administrator <admin@example.com>
2020-04-01 13:52:41 +0800

    $comment

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#删除无效的image
+docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker stop
+docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm
+docker images|grep none|awk '{print $3 }'|xargs docker rmi
+git checkout .
+git pull
+docker-compose up -d --build
